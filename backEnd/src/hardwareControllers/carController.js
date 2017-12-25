5ed2db6aeb640e4e01aa30b3d79359569be11e6b
henrysu <hangsu1985@gmail.com>
2017-12-25 11:02:38 +0800

    henry, add move forward and move backward method

--- backEnd/src/hardwareControllers/carController.js
+++ backEnd/src/hardwareControllers/carController.js
@@ -1,18 +1,28 @@
 let gpio = require("pi-gpio");
-
-let car = {
-    motors:{
-        leftFront:11,
-        leftBack:12,
-        rightFront:15,
-        rightBack:16
-    },
-
-    init: func
-};
+let async = require("async");
+require("./motorController");
 
 class CarController{
-    constructor(){
-        
+    constructor(leftFrontWheel, rightFrontWheel, leftBackWheel, rightBackWeel){
+        this.leftBack = leftBackWheel;
+        this.leftFront = leftFrontWheel;
+        this.rightBack = rightBackWeel;
+        this.rightFront = rightFrontWheel;
+        this.wheels = [leftBackWheel, leftFrontWheel, rightFrontWheel,rightBack];
     }
+
+    //todo: use async and make all wheels move simutaniously
+    moveForward(){
+        for(let wheel in wheels){
+            wheel.forward();
+        }
+    }
+
+    moveBackword(){
+        for(let wheel in wheels){
+            wheel.backward();
+        }
+    }
+    turnLeft(){}
+    turnRight(){}
 }
--- backEnd/src/hardwareControllers/motorController.js
+++ backEnd/src/hardwareControllers/motorController.js
@@ -2,6 +2,11 @@ class MotorController {
     constructor(){
 
     }
+    forward(){
 
+    }
+    backward(){
+
+    }
 
 }
