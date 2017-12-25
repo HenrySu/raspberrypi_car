a1e0b092a7ec80aff75ae4cdf657db6f17081f27
henrysu <hangsu1985@gmail.com>
2017-12-25 11:21:30 +0800

    henry, implement forward and backward methods in motor controller

--- backEnd/src/hardwareControllers/motorController.js
+++ backEnd/src/hardwareControllers/motorController.js
@@ -1,12 +1,15 @@
-class MotorController {
-    constructor(){
+let gpio = require("pi-gpio");
 
+class MotorController {
+    constructor(pinNum){
+        this.pinNum = pinNum;
+        gpio.open(this.pinNum, "output");
     }
     forward(){
-
+        gpio.write(this.pinNum, 1);        
     }
     backward(){
-
+        gpio.write(this.pinNum, 0);        
     }
 
 }
