88b0f6f2c80eb46c32c3d8e96358e2c6ff5c2969
henrysu <hangsu1985@gmail.com>
2017-12-28 12:19:41 +0800

    henry, update stop method, set duty cycle to 0

--- backEnd/src/hardwareControllers/wheelMotorController.js
+++ backEnd/src/hardwareControllers/wheelMotorController.js
@@ -9,20 +9,23 @@ class WheelMotorController {
         this.forwardGpio = new Gpio(wheelConfig.forwardPinNum, {mode:Gpio.OUTPUT});
         this.backwardGpio = new Gpio(wheelConfig.backwardPinNum, {mode:Gpio.OUTPUT});
         this.pwmGpio = new Gpio(wheel.wheelConfig.pwmPinNum, {mode:Gpio.OUTPUT});
-    }
-    
+        this.dutyCycle = 20;
+    }    
+
     forward(){
+        this.pwmGpio.pwmWrite(this.dutyCycle);
         this.forwardGpio.digitalWrite(1);
         this.backwardGpio.digitalWrite(0);
     }
     backward(){
+        this.pwmGpio.pwmWrite(this.dutyCycle);
         this.forwardGpio.digitalWrite(0);
         this.backwardGpio.digitalWrite(1);
     }
     stop(){
         this.forwardGpio.digitalWrite(0);
         this.backwardGpio.digitalWrite(0);
-        this.pwmGpio.digitalWrite(0);
+        this.pwmGpio.pwmWrite(0);
     }
 
 }
