22a6e684e15c4c6f5414872739d1ab8b161678ae
henrysu <hangsu1985@gmail.com>
2017-12-31 16:12:13 +0800

    henry, add speed property

--- backEnd/src/hardwareControllers/wheelMotorController.js
+++ backEnd/src/hardwareControllers/wheelMotorController.js
@@ -1,33 +1,41 @@
 const Gpio = process.env.NODE_ENV !== "production" ? 
     require("pigpio-mock").Gpio : 
     require("pigpio").Gpio;
+const math = require('node-math');
 
 class WheelMotorController {
     //wheelConfig -> WheelMotorController
     //wheelConfig example {forwardPinNum: 22, backwardPinNum: 27, pwmPinNum: 18}
     constructor(wheelConfig){
-        this.forwardGpio = new Gpio(wheelConfig.forwardPinNum, {mode:Gpio.OUTPUT});
-        this.backwardGpio = new Gpio(wheelConfig.backwardPinNum, {mode:Gpio.OUTPUT});
-        this.pwmGpio = new Gpio(wheel.wheelConfig.pwmPinNum, {mode:Gpio.OUTPUT});
-        this.dutyCycle = 20;
+        this.setupGpios(wheelConfig);
+        this.Speed = 0;        
     }    
+    setupGpios(wheelConfig) {
+        this._forwardGpio = new Gpio(wheelConfig.forwardPinNum, { mode: Gpio.OUTPUT });
+        this._backwardGpio = new Gpio(wheelConfig.backwardPinNum, { mode: Gpio.OUTPUT });
+        this._pwmGpio = new Gpio(wheel.wheelConfig.pwmPinNum, { mode: Gpio.OUTPUT });
+    }
 
+    get Speed() {
+        return this._dutyCycle;
+    }
+    set Speed(dutyCycle){
+        this._dutyCycle = dutyCycle;
+        this._pwmGpio.pwmWrite(this._dutyCycle);
+    }
     forward(){
-        this.pwmGpio.pwmWrite(this.dutyCycle);
-        this.forwardGpio.digitalWrite(1);
-        this.backwardGpio.digitalWrite(0);
+        this._forwardGpio.digitalWrite(1);
+        this._backwardGpio.digitalWrite(0);
     }
     backward(){
-        this.pwmGpio.pwmWrite(this.dutyCycle);
-        this.forwardGpio.digitalWrite(0);
-        this.backwardGpio.digitalWrite(1);
+        this._forwardGpio.digitalWrite(0);
+        this._backwardGpio.digitalWrite(1);
     }
     stop(){
-        this.forwardGpio.digitalWrite(0);
-        this.backwardGpio.digitalWrite(0);
-        this.pwmGpio.pwmWrite(0);
+        this._forwardGpio.digitalWrite(0);
+        this._backwardGpio.digitalWrite(0);
+        this.Speed = 0;
     }
-
 }
 
 module.exports = WheelMotorController;
