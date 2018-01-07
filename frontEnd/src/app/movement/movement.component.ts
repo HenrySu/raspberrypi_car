b1129d94835ae5248e54c262b5c87e0a94ddad1b
henrysu <hangsu1985@gmail.com>
2018-01-07 21:04:13 +0800

    henry, wire forward() from component to carController service

--- frontEnd/src/app/movement/movement.component.ts
+++ frontEnd/src/app/movement/movement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarControllerService } from '../car-controller.service';
 
 @Component({
   selector: 'app-movement',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private carController: CarControllerService) { }
 
   ngOnInit() {
   }
 
+  forward(){
+    this.carController.forward();
+  }
 }
